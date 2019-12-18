87db0b0b36343c84f2152f684007149cae5c3a76
kiennd <kiennd@healthchain.vn>
2019-12-18 17:11:52 +0700

    d

--- static/app/chungtu/bangkiemdinh/js/ModelView.js
+++ static/app/chungtu/bangkiemdinh/js/ModelView.js
@@ -240,6 +240,25 @@ define(function (require) {
 								drawOnCanvas(file);   // see Example 6
 								//displayAsImage(file); // see Example 7
 							};
+							function drawOnCanvas(file) {
+								var reader = new FileReader();
+								reader.onload = function (e) {
+									var dataURL = e.target.result,
+										c = document.querySelector('canvas'), // see Example 4
+										ctx = c.getContext('2d'),
+										img = new Image();
+
+									img.onload = function () {
+										c.width = img.width;
+										c.height = img.height;
+										ctx.drawImage(img, 0, 0);
+									};
+
+									img.src = dataURL;
+								};
+
+								reader.readAsDataURL(file);
+							}
 							
 			self.bindEventSelect();
 			var id = this.getApp().getRouter().getParam("id");
