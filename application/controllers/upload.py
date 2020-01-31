218bcba816348eaa47c85fa21309e344af8a4810
kiennd <kiennd@healthchain.vn>
2020-01-31 18:29:35 +0700

    upda api uplaod

--- application/controllers/upload.py
+++ application/controllers/upload.py
@@ -119,29 +119,31 @@ async def upload_file(request):
 
     print('data', data)
 
+    img_uri = data['imgURI']
+
     # if request.method == 'POST':
 
-    #     rand = ''.join(random.choice(string.digits) for _ in range(15))
-    #     file_name = os.path.splitext(file.name)[0]
+    rand = ''.join(random.choice(string.digits) for _ in range(15))
+    file_name = os.path.splitext(file.name)[0]
 
-    #     extname = os.path.splitext(file.name)[1]
-    #     newfilename = file_name + extname 
+    extname = os.path.splitext('xxxxxx')
+    newfilename = file_name + extname 
 
-    #     new_filename = newfilename.replace(" ", "_")
-    #     async with aiofiles.open(fsroot + new_filename, 'wb+') as f:
-    #         await f.write(file.body)
+    new_filename = newfilename.replace(" ", "_")
+    async with aiofiles.open(fsroot + new_filename, 'wb+') as f:
+        await f.write(img_uri)
 
-    #     return json({
-    #             "error_code": "OK",
-    #             "error_message": "successful",
-    #             "id":rand,
-    #             "link":url  + "/" + new_filename,
-    #             "filename":newfilename,
-    #             "filename_organization":file_name,
-    #             "extname":extname
-    #         }, status=200)
-    
     return json({
-        "error_code": "Upload Error",
-        "error_message": "Could not upload file to store"
-    }, status=520)
+            "error_code": "OK",
+            "error_message": "successful",
+            "id":rand,
+            "link":url  + "/" + new_filename,
+            "filename":newfilename,
+            "filename_organization":file_name,
+            "extname":extname
+        }, status=200)
+    
+    # return json({
+    #     "error_code": "Upload Error",
+    #     "error_message": "Could not upload file to store"
+    # }, status=520)
